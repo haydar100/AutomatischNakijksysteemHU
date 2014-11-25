fe7ea2413522b53d8fff2b1028f5032e89340327
haydar100 <haydar.yilmaz@student.hu.nl>
2014-11-25 13:45:08 +0100

    added setup() method, and created some objects for testing.

--- AutomatischNakijken/src/main/java/fnt/hu/nl/automatischNakijken/Main.java
+++ AutomatischNakijken/src/main/java/fnt/hu/nl/automatischNakijken/Main.java
@@ -1,30 +1,46 @@
 package fnt.hu.nl.automatischNakijken;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import org.hibernate.*;
 
 import fnt.hu.nl.automatischNakijken.Data.HibernateUtil;
 import fnt.hu.nl.automatischNakijken.Domain.Course;
 import fnt.hu.nl.automatischNakijken.Domain.CoursePeriod;
+import fnt.hu.nl.automatischNakijken.Domain.Group;
+import fnt.hu.nl.automatischNakijken.Domain.Instructor;
+import fnt.hu.nl.automatischNakijken.Domain.Solution;
+import fnt.hu.nl.automatischNakijken.Domain.Student;
+import fnt.hu.nl.automatischNakijken.Domain.Class;
+import fnt.hu.nl.automatischNakijken.Domain.TeachingAssistant;
 
 /**
- * Hello world!
+ * Main class Automatisch nakijksysteem
  *
  */
 public class Main 
 {
-    public static void main( String[] args )
-    {
-    	Course course = new Course("CSCHERP", "TICT-ACS");	
+	public static void setup() {
+		Course course = new Course("CSCHERP", "TICT-ACS");
+    	TeachingAssistant teachingAssistant = new TeachingAssistant("Roy", "Straub", "Roy Straub", "Roy.straub@student.hu.nl");
+    	Instructor instructor = new Instructor("Esther", "Stappen", "Esther van der Stappen", "Esther.vanderstappen@hu.nl");
+    	Student student = new Student("Haydar", "Yilmaz", "Haydar Yilmaz", "haydar.yilmaz@gmail.com");
+    	Solution solution = new Solution();
+    	ArrayList<Solution> solutions = new ArrayList<Solution>();
+    	ArrayList<Student> students = new ArrayList<Student>();
+    	students.add(student);
+    	solutions.add(solution);
     	CoursePeriod coursePeriod = new CoursePeriod(Calendar.getInstance(), Calendar.getInstance(), course);
-    	
+    	Class c1 = new Class("Software Engineering C1", coursePeriod);
+    	Group group = new Group(1, c1, solutions, students);
     	Calendar c= Calendar.getInstance(); 
     	c.set(2014, 3, 1);
     	Calendar c2= Calendar.getInstance(); 
     	c2.set(2014, 6, 1);
     	CoursePeriod cp2 = new CoursePeriod(c, c2, course);
-    	
     	Session session = HibernateUtil.getSessionFactory().openSession();
     	session.beginTransaction();
     	session.save(course);
@@ -35,7 +51,24 @@ public class Main
     	session.beginTransaction();
     	session.save(cp2);
     	session.getTransaction().commit();
-        System.out.println( "Committed....." + course.getName()+ " " + course.getCode());
+    	session.beginTransaction();
+    	session.save(solution);
+    	session.getTransaction().commit();
+    	session.beginTransaction();
+    	session.save(student);
+    	session.getTransaction().commit();
+    	session.save(c1);
+    	session.getTransaction().commit();
+    	session.beginTransaction();
+    	session.save(group);
+    	session.getTransaction().commit();
+    	System.out.println("Session commited....");
         session.close();
+        System.out.println("Session closed.... !");
+
+	}
+    public static void main( String[] args )
+    {
+    	setup();
     }
 }
