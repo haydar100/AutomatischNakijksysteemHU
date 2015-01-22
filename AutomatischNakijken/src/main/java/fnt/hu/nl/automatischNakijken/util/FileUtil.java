2d3b712353ad9952a9f75a4555871f3f208e5723
Haydar Yilmaz <Haydar.yilmaz@gmail.com>
2015-01-22 07:35:54 +0100

    added methods to FileUtil to check fileSizes (to check if file has proper size * Security *)

--- AutomatischNakijken/src/main/java/fnt/hu/nl/automatischNakijken/util/FileUtil.java
+++ AutomatischNakijken/src/main/java/fnt/hu/nl/automatischNakijken/util/FileUtil.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,6 +33,28 @@ public class FileUtil {
 			_logger.log(Level.WARNING, "Reading content from file at location " + file.getAbsolutePath() + " failed");
 			return null;
 		}
+	}
+	
+	
+	public static long getSize(File file) {
+	    long size;
+	    if (file.isDirectory()) {
+	        size = 0;
+	        for (File child : file.listFiles()) {
+	            size += getSize(child);
+	        }
+	    } else {
+	        size = file.length();
+	    }
+	    return size;
+	}
+	
+	public static String getReadableSize(long size) {
+	    if(size <= 0) return "0";
+	    final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
+	    int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+	    return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups))
+	            + " " + units[digitGroups];
 	}
 	
 	public static void writeContentToFile(File file, String content){
